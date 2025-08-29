3b47775b9f58216c4b16aa08b33464786da512c0
Benitez_Lucas <benitezlucas51@gmail.com>
2025-08-29 00:26:20 -0300

    Creación del repositorio

---
+++ SistemaVenta/CapaPresentacion/PerfilVendedor_Inicio.Designer.cs
@@ -0,0 +1,191 @@
+﻿namespace CapaPresentacion
+{
+    partial class PerfilVendedor_Inicio
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.BtnClientes = new System.Windows.Forms.Button();
+            this.BtnHistorial = new System.Windows.Forms.Button();
+            this.BtnCatalogo = new System.Windows.Forms.Button();
+            this.BtnVentas = new System.Windows.Forms.Button();
+            this.BtnInicio = new System.Windows.Forms.Button();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.BtnSalir = new System.Windows.Forms.Button();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(33)))), ((int)(((byte)(33)))), ((int)(((byte)(33)))));
+            this.panel1.Controls.Add(this.BtnClientes);
+            this.panel1.Controls.Add(this.BtnHistorial);
+            this.panel1.Controls.Add(this.BtnCatalogo);
+            this.panel1.Controls.Add(this.BtnVentas);
+            this.panel1.Controls.Add(this.BtnInicio);
+            this.panel1.Location = new System.Drawing.Point(0, 2);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(200, 748);
+            this.panel1.TabIndex = 0;
+            // 
+            // BtnClientes
+            // 
+            this.BtnClientes.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.BtnClientes.FlatAppearance.BorderSize = 0;
+            this.BtnClientes.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(128)))));
+            this.BtnClientes.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Maroon;
+            this.BtnClientes.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.BtnClientes.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BtnClientes.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.BtnClientes.Location = new System.Drawing.Point(6, 318);
+            this.BtnClientes.Name = "BtnClientes";
+            this.BtnClientes.Size = new System.Drawing.Size(194, 57);
+            this.BtnClientes.TabIndex = 6;
+            this.BtnClientes.Text = "Clientes";
+            this.BtnClientes.UseVisualStyleBackColor = true;
+            // 
+            // BtnHistorial
+            // 
+            this.BtnHistorial.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.BtnHistorial.FlatAppearance.BorderSize = 0;
+            this.BtnHistorial.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(128)))));
+            this.BtnHistorial.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Maroon;
+            this.BtnHistorial.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.BtnHistorial.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BtnHistorial.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.BtnHistorial.Location = new System.Drawing.Point(6, 255);
+            this.BtnHistorial.Name = "BtnHistorial";
+            this.BtnHistorial.Size = new System.Drawing.Size(194, 57);
+            this.BtnHistorial.TabIndex = 5;
+            this.BtnHistorial.Text = "Historial";
+            this.BtnHistorial.UseVisualStyleBackColor = true;
+            // 
+            // BtnCatalogo
+            // 
+            this.BtnCatalogo.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.BtnCatalogo.FlatAppearance.BorderSize = 0;
+            this.BtnCatalogo.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(128)))));
+            this.BtnCatalogo.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Maroon;
+            this.BtnCatalogo.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.BtnCatalogo.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BtnCatalogo.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.BtnCatalogo.Location = new System.Drawing.Point(6, 192);
+            this.BtnCatalogo.Name = "BtnCatalogo";
+            this.BtnCatalogo.Size = new System.Drawing.Size(194, 57);
+            this.BtnCatalogo.TabIndex = 4;
+            this.BtnCatalogo.Text = "Catálogo";
+            this.BtnCatalogo.UseVisualStyleBackColor = true;
+            // 
+            // BtnVentas
+            // 
+            this.BtnVentas.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.BtnVentas.FlatAppearance.BorderSize = 0;
+            this.BtnVentas.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(128)))));
+            this.BtnVentas.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Maroon;
+            this.BtnVentas.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.BtnVentas.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BtnVentas.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.BtnVentas.Location = new System.Drawing.Point(6, 138);
+            this.BtnVentas.Name = "BtnVentas";
+            this.BtnVentas.Size = new System.Drawing.Size(194, 57);
+            this.BtnVentas.TabIndex = 3;
+            this.BtnVentas.Text = "Ventas";
+            this.BtnVentas.UseVisualStyleBackColor = true;
+            // 
+            // BtnInicio
+            // 
+            this.BtnInicio.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.BtnInicio.FlatAppearance.BorderSize = 0;
+            this.BtnInicio.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(128)))));
+            this.BtnInicio.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Maroon;
+            this.BtnInicio.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.BtnInicio.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BtnInicio.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.BtnInicio.Location = new System.Drawing.Point(6, 75);
+            this.BtnInicio.Name = "BtnInicio";
+            this.BtnInicio.Size = new System.Drawing.Size(194, 57);
+            this.BtnInicio.TabIndex = 2;
+            this.BtnInicio.Text = "Inicio";
+            this.BtnInicio.UseVisualStyleBackColor = true;
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(39)))), ((int)(((byte)(39)))), ((int)(((byte)(39)))));
+            this.panel2.Location = new System.Drawing.Point(200, 77);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(1158, 660);
+            this.panel2.TabIndex = 1;
+            // 
+            // BtnSalir
+            // 
+            this.BtnSalir.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.BtnSalir.FlatAppearance.BorderSize = 0;
+            this.BtnSalir.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(128)))));
+            this.BtnSalir.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Maroon;
+            this.BtnSalir.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.BtnSalir.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BtnSalir.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.BtnSalir.Location = new System.Drawing.Point(1151, 12);
+            this.BtnSalir.Name = "BtnSalir";
+            this.BtnSalir.Size = new System.Drawing.Size(194, 57);
+            this.BtnSalir.TabIndex = 3;
+            this.BtnSalir.Text = "Salir";
+            this.BtnSalir.UseVisualStyleBackColor = true;
+            this.BtnSalir.Click += new System.EventHandler(this.BtnSalir_Click);
+            // 
+            // PerfilVendedor
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(33)))), ((int)(((byte)(33)))), ((int)(((byte)(33)))));
+            this.ClientSize = new System.Drawing.Size(1370, 749);
+            this.Controls.Add(this.BtnSalir);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.KeyPreview = true;
+            this.Name = "PerfilVendedor";
+            this.Text = "Vendedor";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.PerfilVendedor_KeyDown);
+            this.panel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Button BtnInicio;
+        private System.Windows.Forms.Button BtnHistorial;
+        private System.Windows.Forms.Button BtnCatalogo;
+        private System.Windows.Forms.Button BtnVentas;
+        private System.Windows.Forms.Button BtnClientes;
+        private System.Windows.Forms.Button BtnSalir;
+    }
+}
+
---
+++ SistemaVenta/CapaPresentacion/PerfilVendedor_Inicio.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CapaPresentacion
+{
+    public partial class PerfilVendedor_Inicio : Form
+    {
+        public PerfilVendedor_Inicio()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnSalir_Click(object sender, EventArgs e)
+        {
+            Application.Exit(); // cierra toda la aplicación
+        }
+
+        private void PerfilVendedor_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Control && e.KeyCode == Keys.S) // Detecta Ctrl + S
+            {
+                Application.Exit(); 
+            }
+        }
+    }
+}
